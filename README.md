2302fffcc1e4035cb37ddfe03673cd067424cb9d
mt <139642124+mt6x@users.noreply.github.com>
2024-07-09 02:20:03 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# Art
+
+Art is a package that allows a user to display colourful text inside of their terminal. It is written in Python, and is very simple to use and fast working.
+
+## Syntax
+
+### Defining an RGB colour.
+
+To clarify, an RGB colour is made of three parts. Red, Green and Blue. Each value ranges from 0 - 255, and it is these parts mixed together which is what creates our final colour value. So when defining an RGB colour using Art, it is very simple to do so.
+
+```py
+colour = RGB(255,0,255) # Pink
+```
+
+You need to provided a minimum of **__two__** colours to the colour text function, else your gradient will simply not render.
+
+## Displaying colourful text.
+
+When displaying colourful text, we want to ensure that we have all of our colours ready, and the provided text that we want to glamourise.
+
+An example of how we would do this is written below.
+
+```py
+# Define both of our colours.
+colour1 = RGB(255,0,255)
+colour2 = RGB(0,255,255)
+
+# Write our text to the terminal.
+print(colourString("This is what I want to be coloured", [colour1, colour2]))
+```
+
+This would in turn output the text with a gradient effect on top of it. Please note, you can use more than two colours, infact; there is no limit to how many colours you can use. It is up to you to personalise how you want your text to look with Art.
---
+++ art.py
@@ -0,0 +1,74 @@
+# The RGB class allows us to store the colours in a usable manor.
+class RGB:
+    def __init__(self, r: int, g: int, b: int) -> None:
+        self.r = r
+        self.g = g
+        self.b = b
+    
+    # Validate if the colour has been defined properly.
+    def valid(self) -> bool:
+        return (self.r > 0 and self.r <= 255) or (self.g > 0 and self.g <= 255) or (self.b > 0 and self.b <= 255)
+    
+    # Convert the RGB class into an ANSI escape code.
+    def ansi(self) -> str:
+        return f"\x1b[38;2;{self.r};{self.g};{self.b}m"
+
+    # Represent the class with the defined r,g,b methods.
+    def __repr__(self):
+        return f"rgb({self.r},{self.g},{self.b})"
+
+# Create a linear RGB colour gradient.
+def linear_gradient(colours: list, steps: int) -> list:
+   # Check if each colour is valid.
+   for colour in colours:
+    try:
+        # Check if the current colour is valid.
+        if not colour.valid():
+            return None
+    except:
+        # The provided item is not an RGB object.
+        return None
+
+    # Check if the user has followed the correct syntax.
+    if len(colours) < 2:
+        return None
+
+    # Check if there is any requirement to run the algorithm.
+    if steps <= len(colours) :
+        return colours
+
+    # Contain the output list for the colour steps that will be created.
+    output = []
+
+    # Contain the total colour segments that we will be using.
+    segments = len(colours) - 1
+
+    # Calculate the amount of colour steps per segment.
+    stepsPerSegment = steps // segments
+
+    for i in range(segments):
+        # Contain the current step value.
+        currentSegment = stepsPerSegment + 1 if i == segments - 1 else stepsPerSegment
+
+        for j in range(currentSegment):
+            # new = start + change * (end - start)
+            output.append(RGB(int(colours[i].r + (j / (currentSegment - 1)) * (colours[i+1].r - colours[i].r)),int(colours[i].g + (j / (currentSegment - 1)) * (colours[i+1].g - colours[i].g)),int(colours[i].b + (j / (currentSegment - 1)) * (colours[i+1].b - colours[i].b))))
+    
+    return output
+
+# Convert a provided string into a gradient string.
+def colourString(string: str, colours: list) -> str:
+    # Create the gradient colours.
+    colours = linear_gradient(colours, len(string))
+
+    # Check if the user has provided a correct colour array.
+    if colours == None:
+        return string
+
+    # Contain the output for the string after the gradient has been applied.
+    output = []
+
+    for i in range(len(colours)-1):
+        output.append(f"{colours[i].ansi()}{string[i]}")
+    
+    return "".join(output)
